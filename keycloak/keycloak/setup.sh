18ad257f81aaef7250a764532857fce3329f54cb
nozomi <meruto@syou551.dev>
2025-02-02 05:45:26 +0000

    fix setup.sh

--- keycloak/keycloak/setup.sh
+++ keycloak/keycloak/setup.sh
@@ -1,7 +1,15 @@
-#/bin/bash
+#!/bin/bash
 
 cd /opt/keycloak/bin
 
+./kc.sh start-dev &
+
+sleep 30
 ./kcadm.sh config credentials --server http://localhost:8080 --realm master --user admin --password admin
 
 ./kcadm.sh update realms/master -s sslRequired=NONE
+
+while true
+do
+sleep 1000
+done
